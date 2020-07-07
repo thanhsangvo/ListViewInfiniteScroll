6f111e01e86ee61c5023f93235c85a9ac21857b2
thanhsangvo <pl.thanhsang0710@gmail.com>
2020-07-07 14:34:38 +0700

    Initial Commit

---
+++ ListViewInfiniteScroll/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListViewInfiniteScroll
+//
+//  Created by Võ Thanh Sang on 7/7/20.
+//  Copyright © 2020 Võ Thanh Sang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
